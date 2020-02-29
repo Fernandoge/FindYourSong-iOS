ab7d7dc7a5aa9885ded02cc998a3e17e30c2b204
Fernandoge <fernando.gp101@gmail.com>
2020-02-28 22:27:35 -0300

    URLSession to fetch songs from Itunes API

--- FindYourSong/FindYourSong/APIs/Itunes/ItunesManager.swift
+++ FindYourSong/FindYourSong/APIs/Itunes/ItunesManager.swift
@@ -17,15 +17,58 @@ protocol ItunesManagerDelegate {
     func itunesManager(itunesManager: ItunesManagerProtocol, didFetchSongs songs: [Song])
 }
 
-class ItunesManager: ItunesManagerProtocol {
+class ItunesManager: NSObject, ItunesManagerProtocol, URLSessionDataDelegate, URLSessionTaskDelegate {
     
     let itunesURL = "https://itunes.apple.com/search?mediaType=music"
     
     var delegate: ItunesManagerDelegate?
 
+    lazy var session: URLSession = {
+        let config = URLSessionConfiguration.default
+        if let delegate = self.delegate {
+            return URLSession(configuration: config, delegate: self, delegateQueue: OperationQueue.main)
+        } else {
+            return URLSession(configuration: config)
+        }
+    }()
+    
+    private var dataTask: URLSessionDataTask!
+    var results = [String: NSMutableData]()
+    
     func fetchSongs(songName: String) {
         let urlString = "\(itunesURL)&term=\(songName)"
         let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters:  .urlQueryAllowed)!
         let url = URL(string: encodedUrlString)!
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        let key = String(dataTask.taskIdentifier)
+        var result = results[key]
+        if result == nil {
+            result = NSMutableData(data: data)
+            results[key] = result
+        } else {
+            result?.append(data)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let _ = error {
+            delegate?.itunesManager(itunesManager: self, didFetchSongs: [])
+        } else {
+            let key = String(task.taskIdentifier)
+            if let result = results[key] as Data? {
+                if let songs = self.parseJSONToSong(songsData: result) {
+                    delegate?.itunesManager(itunesManager: self, didFetchSongs: songs)
+                }
+            } else {
+                delegate?.itunesManager(itunesManager: self, didFetchSongs: [])
+            }
+        }
+    }
+    
+    func parseJSONToSong(songsData: Data) -> [Song]? {
+        //
+        return [Song(name: "placeholder", artistName: "placeholder", albumNameCensored: "placeholder", albumArtworkUrl100: "placeholder", previewUrl: "placeholder")]
     }
 }
